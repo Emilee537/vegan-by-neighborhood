349f6c7f07aec38218bc347bb7316d025526472b
Emilee537 <emileejlindner@gmail.com>
2019-05-19 22:17:28 -0400

    reformatted json file for map

---
+++ js/index.js
@@ -0,0 +1,22 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZW1pbGVlNTM3IiwiYSI6ImNqdnB5ODU4djJlaWY0M3FvMmN6cWFkNXMifQ.2CMwBgHk7i0Od2GcX6nAdw';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/emilee537/cjvvm365d4bqz1cs2hmn1gxdt'
+});
+map.on('click', function(e) {
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ['vegan-restaurants'] // replace this with the name of the layer
+  });
+
+  if (!features.length) {
+    return;
+  }
+
+  var feature = features[0];
+
+  var popup = new mapboxgl.Popup({ offset: [0, -15] })
+    .setLngLat(feature.geometry.coordinates)
+    .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
+    .setLngLat(feature.geometry.coordinates)
+    .addTo(map);
+});
